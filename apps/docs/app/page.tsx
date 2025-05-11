098596aed4024aabae319acfa7932cf90f24e697
Prabhat Dwivedi <prabhatdwivedi411@icloud.com>
2025-05-11 20:15:03 +0530

    initial setup

---
+++ apps/docs/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Initial Commit</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ apps/docs/next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ["@amurex/ui"],
+};
+
+export default nextConfig;
