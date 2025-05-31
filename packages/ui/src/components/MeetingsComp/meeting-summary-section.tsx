2a09d9463006d721d4b37c2b5702c0dc0192dc46
Prabhat Dwivedi <prabhatdwivedi411@icloud.com>
2025-05-31 21:18:58 +0530

    added MeetSummarySection for app/meetings/[id] page

---
+++ packages/ui/src/components/MeetingsComp/meeting-summary-section.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import dynamic from "next/dynamic";
+const ReactMarkdown = dynamic(() => import("react-markdown"), {
+  ssr: false,
+});
+
+interface MeetingSummarySectionProps {
+  summary: string;
+  copyMeetingSummaryText: string;
+  handleSummaryClick: () => Promise<void>;
+  setCopyMeetingSummaryText: (text: string) => void;
+}
+
+export const MeetingSummarySection = ({
+  summary,
+  copyMeetingSummaryText,
+  handleSummaryClick,
+  setCopyMeetingSummaryText,
+}: MeetingSummarySectionProps) => {
+  return (
+    <div onClick={handleSummaryClick}>
+      <div className="flex items-center justify-between">
+        <h2 className="text-[#6D28D9] font-medium mb-3 lg:text-xl text-md">
+          Meeting Summary
+        </h2>
+      </div>
+
+      <div className="bg-black rounded-lg p-4 border border-zinc-800">
+        <div className="prose text-zinc-300 lg:text-base text-sm bg-default markdown-body">
+          {summary ? (
+            <ReactMarkdown
+              components={{
+                h3: ({ node, ...props }) => (
+                  <h3 className="mb-1 text-lg font-bold" {...props} />
+                ),
+                p: ({ node, ...props }) => <p className="mb-2" {...props} />,
+                ul: ({ node, ...props }) => (
+                  <ul className="list-disc pl-5 mb-2" {...props} />
+                ),
+                li: ({ node, ...props }) => (
+                  <li className="mb-1 ml-4" {...props} />
+                ),
+                strong: ({ node, ...props }) => (
+                  <strong className="font-bold" {...props} />
+                ),
+              }}
+            >
+              {summary}
+            </ReactMarkdown>
+          ) : (
+            "No meeting notes available."
+          )}
+        </div>
+      </div>
+
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          navigator.clipboard.writeText(summary);
+          setCopyMeetingSummaryText("Copied!");
+          setTimeout(() => setCopyMeetingSummaryText("Copy"), 3000);
+        }}
+        className="mt-4 px-4 py-2 rounded-lg flex items-center justify-center gap-2 text-xs font-medium border border-white/10 bg-zinc-900 text-white transition-all duration-200 hover:border-[#6D28D9]"
+      >
+        <svg
+          width="15"
+          height="15"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M8 4V16C8 17.1046 8.89543 18 10 18H18C19.1046 18 20 17.1046 20 16V7.24853C20 6.77534 19.7893 6.32459 19.4142 6.00001L16.9983 3.75735C16.6232 3.43277 16.1725 3.22205 15.6993 3.22205H10C8.89543 3.22205 8 4.11748 8 5.22205"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+          <path
+            d="M16 4V7H19"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+          <path
+            d="M4 8V20C4 21.1046 4.89543 22 6 22H14C15.1046 22 16 21.1046 16 20"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+        </svg>
+        <span className="text-sm">{copyMeetingSummaryText}</span>
+      </button>
+    </div>
+  );
+};
