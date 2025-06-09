f4eef3e8f20c80e49d8a8e4eb987b9ddc5ba3daf
Prabhat Dwivedi <prabhatdwivedi411@icloud.com>
2025-06-09 15:03:13 +0530

    added Supabase internal package

---
+++ packages/supabase/src/SupabaseClient.ts
@@ -0,0 +1,19 @@
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+
+export const supabase: SupabaseClient = createClient(
+  supabaseUrl,
+  supabaseAnonKey,
+);
+
+export const createSupabaseClient = (accessToken: string): SupabaseClient => {
+  return createClient(supabaseUrl, supabaseAnonKey, {
+    global: {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    },
+  });
+};
