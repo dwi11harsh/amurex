d079b616caca73d71525f6dd1b1e2ed096081970
Prabhat Dwivedi <prabhatdwivedi411@icloud.com>
2025-06-12 22:40:09 +0530

    added components for app/emails page

--- packages/ui/src/components/index.tsx
+++ packages/ui/src/components/index.tsx
@@ -1,8 +1,11 @@
 // Shadcn Components
 export * from "./ui/button";
 export * from "./ui/input";
+export * from "./ui/IconToggle";
+export * from "./ui/card";
 
 // custom
 export * from "./Root";
 export * from "./Navbar";
 export * from "./Callbacks";
+export * from "./MobileWarningBanner";
